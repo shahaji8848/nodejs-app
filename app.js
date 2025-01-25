1fdf30d7546e78f230dfcea5949536d49aa338ac
Shahaji Kadam <shahajik4@gmail.com>
2025-01-25 07:06:54 +0000

    Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js running on localhost!');
+    res.send('Hello from Node.js running on K8!');
 });
 
 const port = process.env.PORT || 3000;
