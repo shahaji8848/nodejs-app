f253596f17a0199c497b9f301897605450ca8a5b
shahaji8848 <155730078+shahaji8848@users.noreply.github.com>
2025-06-06 15:15:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js running on Minikube!123');
+    res.send('Hello from Node.js running on Minikube! - DEMOSK');
 });
 
 const port = process.env.PORT || 3000;
