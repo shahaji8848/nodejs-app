0a3b1fa7e65df57c2337632f6623e8e1605b7758
sk <shahaji@8848digital.com>
2025-06-06 15:22:14 +0530

    demo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js running on Minikube! - DEMOSK');
+    res.send('Hello from Node.js running on Minikube!');
 });
 
 const port = process.env.PORT || 3000;
